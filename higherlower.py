d566a97e31e77b33b46dca5895d4ca86d157428f
Tauseefmalikk <108793050+Tauseefmalikk@users.noreply.github.com>
2024-06-15 11:27:16 +0530

    complete

--- higherlower.py
+++ higherlower.py
@@ -1,4 +1,5 @@
 import random
+import os
 #Display art
 logo = """
     __  ___       __             
@@ -327,38 +328,59 @@ data = [
 
 def format_data(account):
 #format account data into printable format
-    account_name=account_a["name"]
-    account_description=account_a["description"]
-    account_country=account_a["country"]
+    account_name=account["name"]
+    account_description=account["description"]
+    account_country=account["country"]
     return f"{account_name},{account_description} ,{account_country}"
 
+def  check_answer(guess,a_follower,b_follower):
+    if a_follower > b_follower:
+        return guess == "a"
+    else:
+        return guess == "b"
 
+score=0
 
 #generate random account from game data using random module
-account_a=random.choice(data)
 account_b=random.choice(data)
-if account_a == account_b:
+
+game_should_continue=True
+while game_should_continue:
+
+    account_a=account_b
     account_b=random.choice(data)
+    while account_a == account_b:
+        account_b=random.choice(data)
 
 
-print(f"Compare A: {format_data(account_a)}")
+    print(f"Compare A: {format_data(account_a)}")
 
-print(vs)
+    print(vs)
 
-print(f"Against B: {format_data(account_b)}")
-
-
+    print(f"Against B: {format_data(account_b)}")
 
 
+    #ask the user for guess
+
+    guess=input("Who has more followers? Type 'A' or 'B'").lower()
 
 
+    #check if the user is correct
+    a_follower_count=account_a["follower_count"]
+    b_follower_count=account_b["follower_count"]
 
+    is_correcrt=check_answer(guess,a_follower_count,b_follower_count)
 
+    os.system('cls')
+    print(logo)
+    #give user feedback on their guess
 
-
-
-
-
+    if is_correcrt:
+        score  +=1
+        print(f"You're right! Current Score: {score}")
+    else:
+       game_should_continue=False 
+       print(f"Sorry, Thats wrong! Final Score:{score}")
 
 
 
