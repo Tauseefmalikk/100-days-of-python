480a49a2134884ee2e1968e8197dae39cd4a89b3
Tauseefmalikk <108793050+Tauseefmalikk@users.noreply.github.com>
2024-07-16 14:35:14 +0530

    new

--- spirographturtle.py
+++ spirographturtle.py
@@ -1,22 +1,33 @@
-import turtle
 import turtle as t
 import random
 
 directions = [0, 90, 180, 100]
 tee = t.Turtle()
 t.colormode(255)
-tee.pensize(20)
+tee.pensize(2)
 
 
 def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
+    color = (r, g, b)
+    return color
 
 
-for i in range(200):
-    tee.color(random_color())
-    tee.forward(30)
-    tee.setheading(random.choice(directions))
+tee.speed("fastest")
+
+
+def draw(size):
+    for i in range(int(360 / size)):
+        tee.color(random_color())
+        tee.circle(100)
+        current_heading = tee.heading()
+        tee.setheading(current_heading + size)
+        tee.circle(100)
+
+
+draw(6)
+
+screen = t.Screen()
+screen.exitonclick()
